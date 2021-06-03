e35215e304c8852c88470c068963061ba7a23977
Ishani <ischakra@LambdaSrv01.redmond.corp.microsoft.com>
2021-06-03 13:10:53 -0700

    README and cleanup

--- Samples/StreamRecorder/StreamRecorderConverter/detection_utils.py
+++ Samples/StreamRecorder/StreamRecorderConverter/detection_utils.py
@@ -9,7 +9,6 @@ import sys
 class MaskRCNNDetector():
     def __init__(self, config_file, valid_categories=None):
         from detectron2.config import get_cfg
-        # from meshrcnn.config import get_meshrcnn_cfg_defaults
         from detectron2.data import MetadataCatalog
         from detectron2.data.detection_utils import read_image
         from detectron2.engine.defaults import DefaultPredictor
@@ -20,12 +19,8 @@ class MaskRCNNDetector():
         from detectron2.checkpoint import DetectionCheckpointer
         from detectron2.export import Caffe2Tracer
         
-		#self.valid_categories = ['bench', 'chair', 'couch', 'dining table', 'laptop', 'tv'] #'person', 'potted plant'
         cfg = get_cfg()
-        # get_meshrcnn_cfg_defaults(cfg)
         cfg.merge_from_file(config_file)
-        cfg_onnx = cfg.clone()
-        cfg_onnx.MODEL.DEVICE = "cpu"
         cfg.MODEL.ROI_HEADS.NMS_THRESH_TEST = 0.3
         cfg.MODEL.RETINANET.NMS_THRESH_TEST = 0.3
         cfg.freeze()
@@ -37,6 +32,7 @@ class MaskRCNNDetector():
             self.valid_categories = self.category_names
         else:
             self.valid_categories = valid_categories
+
         self.threshold = 0.5
         self.filter_categories = True
         self.filter_scores = True
@@ -48,10 +44,6 @@ class MaskRCNNDetector():
         checkpointer.load(cfg.MODEL.WEIGHTS)
 
         dummy_input = {}
-        # dummy_input['file_name'] = 'test.jpg'
-        # dummy_input['height'] = 400
-        # dummy_input['width'] = 400
-        # dummy_input['image_id'] = 1
         dummy_input['image'] = torch.randn(3, 800, 800)
         first_batch = [dummy_input]
         self.tracer = Caffe2Tracer(cfg_onnx, self.model, first_batch)
@@ -60,11 +52,6 @@ class MaskRCNNDetector():
             [cfg.INPUT.MIN_SIZE_TEST, cfg.INPUT.MIN_SIZE_TEST], cfg.INPUT.MAX_SIZE_TEST
         )
         self.input_format = 'BGR'
-
-    def export_to_onnx(self, onnx_model_path):
-        import torch.onnx
-        onnx_model = self.tracer.export_onnx()
-        onnx.save(onnx_model, onnx_model_path)
 
     def run_on_opencv_image(self, img):
         predictions = self.predictor(img)
@@ -100,77 +87,3 @@ class MaskRCNNDetector():
             return None, None, None
 
         return masks, _names, scores
-        
-    def run_on_opencv_images(self, images_tensor):
-        with torch.no_grad():  # https://github.com/sphinx-doc/sphinx/issues/4258
-            # Apply pre-processing to image.
-            if self.input_format == "RGB":
-                # whether the model expects BGR inputs or RGB
-                images_tensor = images_tensor[:, :, :, ::-1]
-            inputs_list = []                
-            for original_image in images_tensor:
-                height, width = original_image.shape[:2]
-                image = self.transform_gen.get_transform(original_image).apply_image(original_image)
-                image = torch.as_tensor(image.astype("float32").transpose(2, 0, 1))
-                inputs = {"image": image, "height": height, "width": width}
-                inputs_list.append(inputs)
-            predictions = self.model(inputs_list)
-        masks_ms = []
-        boxes_ms = []
-        scores_ms = []
-        names_ms = []                
-        colors_ms = []
-        for pred in predictions:
-            instances = pred["instances"].to(torch.device("cpu"))
-            if len(instances) == 0:
-                continue
-            
-            masks = instances.pred_masks.cpu().numpy()
-            boxes = instances.pred_boxes.tensor.int().cpu().numpy()
-            scores = instances.scores
-            labels = instances.pred_classes.int().cpu().numpy()
-            classes = instances.pred_classes.cpu()
-            if self.category_names:
-                _names = np.array([self.category_names[i] for i in classes])
-            if self.category_colors:
-                _colors = np.array([self.category_colors[i] for i in classes])
-            # score based validation
-            valid_scores = scores > self.threshold
-            # label based validation
-            valid_categories = []
-            for _name in _names:
-                if _name in self.valid_categories:
-                    valid_categories.append(True)
-                else:
-                    valid_categories.append(False)
-            valid_positions = []
-            for mask in masks:
-                if mask[int(height/2), int(width/2)] == True:
-                    valid_positions.append(True)
-                else:
-                    valid_positions.append(False)
-            
-            valid_ids = valid_scores.numpy() * np.array(valid_categories) * np.array(valid_positions)
-            
-            masks = masks[valid_ids,:]
-            boxes = boxes[valid_ids,:]
-            scores = scores[valid_ids]
-            _names = _names[valid_ids]
-            _colors = _colors[valid_ids]
-            
-            if len(scores) > 1:
-                id = np.argmax(scores)
-                masks = masks[id]
-                boxes = boxes[id]
-                scores = scores[id]
-                _names = _names[id]
-                _colors = _colors[id]
-            elif len(scores) == 0:
-                    continue
-
-            masks_ms.append(masks)
-            names_ms.append(_names)
-            colors_ms.append(_colors)
-            scores_ms.append(scores)
-
-        return masks_ms, names_ms, scores_ms, colors_ms
--- Samples/StreamRecorder/StreamRecorderConverter/run_detector.py
+++ Samples/StreamRecorder/StreamRecorderConverter/run_detector.py
@@ -28,11 +28,13 @@ dest = 'labels'
 basedir = args.recording_path
 pv_dir = join(basedir, f'{source}')
 Path(join(basedir, f'{dest}')).mkdir(exist_ok=True)
-# import pdb; pdb.set_trace()
+
 valid_categories = args.valid_categories.split(',')
 
 config_file = "configs/mask_rcnn_R_50_FPN_inference_acc_test.yaml"
 detector = MaskRCNNDetector(config_file, valid_categories=valid_categories)
+
+# Collect colors of valid categories from COCO palette.
 colors = []
 for vc in valid_categories:
 	colors.append(detector.category_colors[detector.category_names.index(vc)])
@@ -41,32 +43,33 @@ image_files = glob.glob(join(pv_dir,'*.png'))
 image_files = sorted(image_files)
 
 for i, image_file in enumerate(tqdm(image_files)):
-	# print(image_file)
 	output_file = image_file.replace(f'{source}',f'{dest}')
 	img = cv2.imread(image_file)
 
-	output = detector.run_on_opencv_image(img)
+	output_masks, output_category_names, output_scores = detector.run_on_opencv_image(img)
 
-	if output[0] is None:
+	# set mask to zero
+	if output_masks is None:
 		cv2.imwrite(output_file,np.zeros_like(img)*255)
 		continue
 
-	num_detections = len(output[0])
+	num_detections = len(output_masks)
 
 	masks = np.zeros((len(valid_categories) + 1, img.shape[0], img.shape[1]))
 	masks_colored = []
+
+	# for each pixel, keep the label with maximum score
 	for idx in range(0, num_detections):
-		vc_idx = valid_categories.index(output[1][idx]) + 1
-		masks[vc_idx] = masks[vc_idx] + (output[0][idx].astype(np.float))*np.array(output[2][idx])
+		vc_idx = valid_categories.index(output_category_names[idx]) + 1
+		masks[vc_idx] = masks[vc_idx] + (output_masks[idx].astype(np.float))*np.array(output_scores[idx])
 	masks = np.argmax(masks, axis=0)
 	
+	# color the masks according to COCO palette
 	for vc_idx in range(0, len(valid_categories)):
 		idx = masks == vc_idx + 1
 		masks_ = np.zeros((img.shape[0], img.shape[1],3))
 		masks_[idx,0] = colors[vc_idx][0]; masks_[idx,1] = colors[vc_idx][1]; masks_[idx,2] = colors[vc_idx][2]
 		masks_colored.append(masks_)
 	masks_colored = np.array(masks_colored).sum(axis=0)
+
 	cv2.imwrite(output_file,masks_colored)
-
-
-
